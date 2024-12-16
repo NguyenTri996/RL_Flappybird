0767916b52d813cd024a19e7ad40be1f3855004b
NguyenTri996 <nguyentri996@gmail.com>
2024-12-16 12:55:10 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,16 @@
+{"version": "0.2.0",
+"configuration" : [
+    {
+        "name": "Train cartpole1",
+        "type": "debuggy",
+        "request": "launch",
+        "program": "agent.py",
+        "console":"intergratedTerminal",
+        "args": [
+            "cartpole1","--train"
+        ]
+    }]
+
+
+
+}
